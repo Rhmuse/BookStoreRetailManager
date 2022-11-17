7d7344410e95a0b66713afc6965dfe7913b9ec14
Rhett Muse <rhett.muse123@gmail.com>
2022-11-16 18:52:47 -0600

    Add spUserLookup Procedure  and connected it to API

---
+++ BSRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,20 @@
+﻿using BSRMDataManager.Library.Internal.DataAccess;
+using BSRMDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace BSRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "BSRMData");
+
+            return output;
+        }
+    }
+}
---
+++ BSRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using BSRMDataManager.Library.DataAccess;
+using BSRMDataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace BSRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
