274a549db3448fcc06b530db6e0d919c34340ad2
Rhett Muse <rhett.muse123@gmail.com>
2022-11-18 14:34:01 -0600

    Get Add To Cart Working
    Adds the functionality to add items to the cart, get the subtotal, and update quantity.

--- BSRMWPFUserInterface/ViewModels/SalesViewModel.cs
+++ BSRMWPFUserInterface/ViewModels/SalesViewModel.cs
@@ -2,6 +2,7 @@
 using BSRMWPFUserInterface.Library.Models;
 using Caliburn.Micro;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BSRMWPFUserInterface.ViewModels
@@ -40,10 +41,23 @@ namespace BSRMWPFUserInterface.ViewModels
 				NotifyOfPropertyChange(() => Products);
 			}
 		}
+		private ProductModel _selectedProduct;
 
-		private BindingList<ProductModel> _cart;
+		public ProductModel SelectedProduct
+		{
+			get { return _selectedProduct; }
+			set
+			{
+				_selectedProduct = value;
+				NotifyOfPropertyChange(() => SelectedProduct);
+				NotifyOfPropertyChange(() => CanAddToCart);
+			}
+		}
 
-		public BindingList<ProductModel> Cart
+
+		private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+		public BindingList<CartItemModel> Cart
 		{
 			get { return _cart; }
 			set
@@ -53,7 +67,7 @@ namespace BSRMWPFUserInterface.ViewModels
 			}
 		}
 
-		private int _itemQuantity;
+		private int _itemQuantity = 1;
 
 		public int ItemQuantity
 		{
@@ -62,6 +76,7 @@ namespace BSRMWPFUserInterface.ViewModels
 			{
 				_itemQuantity = value;
 				NotifyOfPropertyChange(() => ItemQuantity);
+				NotifyOfPropertyChange(() => CanAddToCart);
 			}
 		}
 
@@ -69,8 +84,13 @@ namespace BSRMWPFUserInterface.ViewModels
 		{
 			get
 			{
-				// TODO - Replace with calculation
-				return "$0.00";
+				decimal subTotal = 0;
+
+				foreach (var item in Cart)
+				{
+					subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+				}
+				return subTotal.ToString("C");
 			}
 		}
 
@@ -100,7 +120,10 @@ namespace BSRMWPFUserInterface.ViewModels
 
 				//Make sure something is selected
 				//Make sure there is an item quantity
-
+				if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+				{
+					output = true;
+				}
 
 				return output;
 			}
@@ -109,7 +132,27 @@ namespace BSRMWPFUserInterface.ViewModels
 
 		public void AddToCart()
 		{
+			CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
+			if (existingItem != null)
+			{
+				existingItem.QuantityInCart += ItemQuantity;
+				Cart.Remove(existingItem);
+				Cart.Add(existingItem);
+			}
+			else
+			{
+				CartItemModel item = new CartItemModel
+				{
+					Product = SelectedProduct,
+					QuantityInCart = ItemQuantity
+				};
+				Cart.Add(item);
+			}
+
+			SelectedProduct.QuantityInStock -= ItemQuantity;
+			ItemQuantity = 1;
+			NotifyOfPropertyChange(() => SubTotal);
 		}
 
 		public bool CanRemoveFromCart
@@ -129,6 +172,7 @@ namespace BSRMWPFUserInterface.ViewModels
 
 		public void RemoveFromCart()
 		{
+			NotifyOfPropertyChange(() => SubTotal);
 
 		}
 
