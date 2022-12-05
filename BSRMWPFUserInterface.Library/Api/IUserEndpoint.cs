86ad12e94dc9abd7b341d6000451958d9f1a81f1
Rhett Muse <rhett.muse123@gmail.com>
2022-12-05 14:05:55 -0600

    Create a User Admin Page and View the users
    Add a UserController and UserModel to BSRMDataManager that accesses users with EntityFramework. Add UserEndpoint to the api. Add UserDisplayView in WPF.

---
+++ BSRMWPFUserInterface.Library/Api/IUserEndpoint.cs
@@ -0,0 +1,11 @@
+﻿using BSRMWPFUserInterface.Library.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BSRMWPFUserInterface.Library.Api
+{
+    public interface IUserEndpoint
+    {
+        Task<List<UserModel>> GetAll();
+    }
+}
